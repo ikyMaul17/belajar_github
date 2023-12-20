8a3ad116191d6e6dcb5a593f62172151f4a65175
rizky <rizkymaualanaj@gmail.com>
2023-12-20 22:58:47 +0700

    mengubah sedikit di console.log

--- fungsi.js
+++ fungsi.js
@@ -1,9 +1,7 @@
 console.log("Fungsi Pertama");
+console.log("Fungsi Pkeduaa");
+console.log("Fungsi ketiga");
+console.log("Fungsi keempat");
 
 const add = (a, b) => a + b;
 console.log(add(1, 2));
-
-i = -5;
-if (i <= 0) {
-  console.log("true");
-}
